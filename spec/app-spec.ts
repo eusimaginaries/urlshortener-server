8b827c9a8b501d320c42a20d2c78a585f8d1fd3c
Euston Lee <lee.yousheng@gmail.com>
2020-06-10 15:50:57 +0800

    updated test to incorporate new changes.

--- spec/app-spec.ts
+++ spec/app-spec.ts
@@ -7,7 +7,7 @@ import { MockDB, MockEngine } from './support/mock';
 describe('Get Entries', () => {
   it('should return pagination result, given 1 record.', async () => {
     // Given
-    const testData: Array<UrlEntry> = [{ id: 'key1', url: 'http://www.example.com' }];
+    const testData: Array<UrlEntry> = [{ id: 'key1', rootKey: 'key', url: 'http://www.example.com' }];
     const db: DB = new MockDB(testData);
     const event: APIGatewayProxyEvent = <APIGatewayProxyEvent>{};
 
@@ -19,7 +19,7 @@ describe('Get Entries', () => {
     const bodyObj: PaginationResult = JSON.parse(res.body);
     expect(bodyObj.numItems).toBe(1);
     expect(bodyObj.lastKey).toBe(undefined);
-    expect(bodyObj.items[0]).toEqual({ id: 'key1', url: 'http://www.example.com' });
+    expect(bodyObj.items[0]).toEqual({ id: 'key1', rootKey: 'key', url: 'http://www.example.com' });
   });
 
   it('should return pagination result, given 0 record.', async () => {
@@ -42,17 +42,17 @@ describe('Get Entries', () => {
   it('should return pagination default size 10, given multiple records.', async () => {
     // Given
     const testData: Array<UrlEntry> = [
-      { id: 'key1', url: 'http://ex1.sample' },
-      { id: 'key2', url: 'http://ex2.sample' },
-      { id: 'key3', url: 'http://ex3.sample' },
-      { id: 'key4', url: 'http://ex4.sample' },
-      { id: 'key5', url: 'http://ex5.sample' },
-      { id: 'key6', url: 'http://ex6.sample' },
-      { id: 'key7', url: 'http://ex7.sample' },
-      { id: 'key8', url: 'http://ex8.sample' },
-      { id: 'key9', url: 'http://ex9.sample' },
-      { id: 'key10', url: 'http://ex10.sample' },
-      { id: 'key11', url: 'http://ex11.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
+      { id: 'key3', rootKey: 'key', url: 'http://ex3.sample' },
+      { id: 'key4', rootKey: 'key', url: 'http://ex4.sample' },
+      { id: 'key5', rootKey: 'key', url: 'http://ex5.sample' },
+      { id: 'key6', rootKey: 'key', url: 'http://ex6.sample' },
+      { id: 'key7', rootKey: 'key', url: 'http://ex7.sample' },
+      { id: 'key8', rootKey: 'key', url: 'http://ex8.sample' },
+      { id: 'key9', rootKey: 'key', url: 'http://ex9.sample' },
+      { id: 'key10', rootKey: 'key', url: 'http://ex10.sample' },
+      { id: 'key11', rootKey: 'key', url: 'http://ex11.sample' },
     ];
     const db: DB = new MockDB(testData);
     const event: APIGatewayProxyEvent = <APIGatewayProxyEvent>{};
@@ -66,25 +66,25 @@ describe('Get Entries', () => {
     expect(bodyObj.numItems).toBe(10);
     expect(bodyObj.lastKey).toBe('key10');
     expect(bodyObj.items).toEqual([
-      { id: 'key1', url: 'http://ex1.sample' },
-      { id: 'key2', url: 'http://ex2.sample' },
-      { id: 'key3', url: 'http://ex3.sample' },
-      { id: 'key4', url: 'http://ex4.sample' },
-      { id: 'key5', url: 'http://ex5.sample' },
-      { id: 'key6', url: 'http://ex6.sample' },
-      { id: 'key7', url: 'http://ex7.sample' },
-      { id: 'key8', url: 'http://ex8.sample' },
-      { id: 'key9', url: 'http://ex9.sample' },
-      { id: 'key10', url: 'http://ex10.sample' }
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
+      { id: 'key3', rootKey: 'key', url: 'http://ex3.sample' },
+      { id: 'key4', rootKey: 'key', url: 'http://ex4.sample' },
+      { id: 'key5', rootKey: 'key', url: 'http://ex5.sample' },
+      { id: 'key6', rootKey: 'key', url: 'http://ex6.sample' },
+      { id: 'key7', rootKey: 'key', url: 'http://ex7.sample' },
+      { id: 'key8', rootKey: 'key', url: 'http://ex8.sample' },
+      { id: 'key9', rootKey: 'key', url: 'http://ex9.sample' },
+      { id: 'key10', rootKey: 'key', url: 'http://ex10.sample' }
     ]);
   });
 
   it('should return pagination size 2, given page size 2 specified.', async () => {
     // Given
     const testData: Array<UrlEntry> = [
-      { id: 'key1', url: 'http://ex1.sample' },
-      { id: 'key2', url: 'http://ex2.sample' },
-      { id: 'key3', url: 'http://ex3.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
+      { id: 'key3', rootKey: 'key', url: 'http://ex3.sample' },
     ];
     const db: DB = new MockDB(testData);
     const event: APIGatewayProxyEvent = <APIGatewayProxyEvent>{ queryStringParameters: <{ [name: string]: string }>{ 'pageSize': '2' } };
@@ -98,18 +98,18 @@ describe('Get Entries', () => {
     expect(bodyObj.numItems).toBe(2);
     expect(bodyObj.lastKey).toBe('key2');
     expect(bodyObj.items).toEqual([
-      { id: 'key1', url: 'http://ex1.sample' },
-      { id: 'key2', url: 'http://ex2.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
     ]);
   });
 
   it('should return next page value, given last key is provided.', async () => {
     // Given
     const testData: Array<UrlEntry> = [
-      { id: 'key1', url: 'http://ex1.sample' },
-      { id: 'key2', url: 'http://ex2.sample' },
-      { id: 'key3', url: 'http://ex3.sample' },
-      { id: 'key4', url: 'http://ex4.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
+      { id: 'key3', rootKey: 'key', url: 'http://ex3.sample' },
+      { id: 'key4', rootKey: 'key', url: 'http://ex4.sample' },
     ];
     const db: DB = new MockDB(testData);
     const event: APIGatewayProxyEvent = <APIGatewayProxyEvent>{ queryStringParameters: <{ [name: string]: string }>{ 'pageSize': '2', 'lastKey': 'key1' } };
@@ -123,18 +123,18 @@ describe('Get Entries', () => {
     expect(bodyObj.numItems).toBe(2);
     expect(bodyObj.lastKey).toBe('key3');
     expect(bodyObj.items).toEqual([
-      { id: 'key2', url: 'http://ex2.sample' },
-      { id: 'key3', url: 'http://ex3.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
+      { id: 'key3', rootKey: 'key', url: 'http://ex3.sample' },
     ]);
   });
 
   it('should return last key undefined, given last record is obtained.', async () => {
     // Given
     const testData: Array<UrlEntry> = [
-      { id: 'key1', url: 'http://ex1.sample' },
-      { id: 'key2', url: 'http://ex2.sample' },
-      { id: 'key3', url: 'http://ex3.sample' },
-      { id: 'key4', url: 'http://ex4.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
+      { id: 'key3', rootKey: 'key', url: 'http://ex3.sample' },
+      { id: 'key4', rootKey: 'key', url: 'http://ex4.sample' },
     ];
     const db: DB = new MockDB(testData);
     const event: APIGatewayProxyEvent = <APIGatewayProxyEvent>{ queryStringParameters: <{ [name: string]: string }>{ 'pageSize': '10', 'lastKey': 'key2' } };
@@ -148,16 +148,16 @@ describe('Get Entries', () => {
     expect(bodyObj.numItems).toBe(2);
     expect(bodyObj.lastKey).toBe(undefined);
     expect(bodyObj.items).toEqual([
-      { id: 'key3', url: 'http://ex3.sample' },
-      { id: 'key4', url: 'http://ex4.sample' },
+      { id: 'key3', rootKey: 'key', url: 'http://ex3.sample' },
+      { id: 'key4', rootKey: 'key', url: 'http://ex4.sample' },
     ]);
   });
 
   it('should return ignore last key, given last key is invalid.', async () => {
     // Given
     const testData: Array<UrlEntry> = [
-      { id: 'key1', url: 'http://ex1.sample' },
-      { id: 'key2', url: 'http://ex2.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
     ];
     const db: DB = new MockDB(testData);
     const event: APIGatewayProxyEvent = <APIGatewayProxyEvent>{ queryStringParameters: <{ [name: string]: string }>{ 'pageSize': '10', 'lastKey': 'wrongkey' } };
@@ -171,8 +171,8 @@ describe('Get Entries', () => {
     expect(bodyObj.numItems).toBe(2);
     expect(bodyObj.lastKey).toBe(undefined);
     expect(bodyObj.items).toEqual([
-      { id: 'key1', url: 'http://ex1.sample' },
-      { id: 'key2', url: 'http://ex2.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
     ]);
   });
 });
@@ -180,8 +180,8 @@ describe('Get Entries', () => {
 it('should return ignore pagesize, given page size less than 1', async () => {
   // Given
   const testData: Array<UrlEntry> = [
-    { id: 'key1', url: 'http://ex1.sample' },
-    { id: 'key2', url: 'http://ex2.sample' },
+    { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+    { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
   ];
   const db: DB = new MockDB(testData);
   const event: APIGatewayProxyEvent = <APIGatewayProxyEvent>{ queryStringParameters: <{ [name: string]: string }>{ 'pageSize': '0' } };
@@ -195,8 +195,8 @@ it('should return ignore pagesize, given page size less than 1', async () => {
   expect(bodyObj.numItems).toBe(2);
   expect(bodyObj.lastKey).toBe(undefined);
   expect(bodyObj.items).toEqual([
-    { id: 'key1', url: 'http://ex1.sample' },
-    { id: 'key2', url: 'http://ex2.sample' },
+    { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+    { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
   ]);
 });
 
@@ -204,8 +204,8 @@ describe('Get Url Entry via shortened url', () => {
   it('should retrieve the entry given exist', async () => {
     // Given
     const testData: Array<UrlEntry> = [
-      { id: 'key1', url: 'http://ex1.sample' },
-      { id: 'key2', url: 'http://ex2.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
     ];
     const db: DB = new MockDB(testData);
     const event: APIGatewayProxyEvent = <APIGatewayProxyEvent>{ pathParameters: <{ [name: string]: string }>{ id: 'key2' } };
@@ -223,8 +223,8 @@ describe('Get Url Entry via shortened url', () => {
   it('should return status 404 given invalid', async () => {
     // Given
     const testData: Array<UrlEntry> = [
-      { id: 'key1', url: 'http://ex1.sample' },
-      { id: 'key2', url: 'http://ex2.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
     ];
     const db: DB = new MockDB(testData);
     const event: APIGatewayProxyEvent = <APIGatewayProxyEvent>{ pathParameters: <{ [name: string]: string }>{ id: 'invalidKey' } };
@@ -241,8 +241,8 @@ describe('Get Url Entry via shortened url', () => {
   it('should return status 403 given id not specified', async () => {
     // Given
     const testData: Array<UrlEntry> = [
-      { id: 'key1', url: 'http://ex1.sample' },
-      { id: 'key2', url: 'http://ex2.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
+      { id: 'key2', rootKey: 'key', url: 'http://ex2.sample' },
     ];
     const db: DB = new MockDB(testData);
     const event: APIGatewayProxyEvent = <APIGatewayProxyEvent>{};
@@ -261,7 +261,7 @@ describe('Create new Url Entry', () => {
   it('should return new entry with status 201 given new url.', async () => {
     // Given
     const testData: Array<UrlEntry> = [
-      { id: 'key1', url: 'http://ex1.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
     ];
     const db: DB = new MockDB(testData);
     const testHash: { [key: string]: string } = <{ [key: string]: string }>{ 'http://ex2.sample': 'ABCDEF' }
@@ -281,7 +281,7 @@ describe('Create new Url Entry', () => {
   it('should return entry with status 200 given url previously saved.', async () => {
     // Given
     const testData: Array<UrlEntry> = [
-      { id: 'key1', url: 'http://ex1.sample' },
+      { id: 'key1', rootKey: 'key', url: 'http://ex1.sample' },
     ];
     const db: DB = new MockDB(testData);
     const testHash: { [key: string]: string } = {};
@@ -301,8 +301,8 @@ describe('Create new Url Entry', () => {
   it('should return entry with incremented id with status 201 given root collision.', async () => {
     // Given
     const testData: Array<UrlEntry> = [
-      { id: 'key10', url: 'http://ex10.sample' },
-      { id: 'key11', url: 'http://ex11.sample' },
+      { id: 'key10', rootKey: 'key', url: 'http://ex10.sample' },
+      { id: 'key11', rootKey: 'key', url: 'http://ex11.sample' },
     ];
     const db: DB = new MockDB(testData);
     const testHash: { [key: string]: string } = <{ [key: string]: string }>{ 'http://ex12.sample': 'key1' };
